bfabb183ed34da37f9a3eda218bc50c4b79a0bec
Dicksiano <dicksianomelo@gmail.com>
2018-03-28 19:51:28 -0300

    Random gen

---
+++ project1/pseudoRandomGenerator.py
@@ -0,0 +1,14 @@
+import random
+import sys
+
+def generate():
+	file = open("input.txt","w") 
+	n = int(sys.argv[1])
+
+	for i in range(n):
+		x = random.randint(0,9)
+		file.write(str(x))
+
+	file.close()
+
+generate()
