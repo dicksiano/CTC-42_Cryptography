91d34f86a92867ee4e5e28d72163510778d52bbc
Dicksiano <dicksianomelo@gmail.com>
2018-03-28 22:05:34 -0300

    Encoding working

--- project1/encode.py
+++ project1/encode.py
@@ -8,13 +8,13 @@ decimal.setcontext(context)
 def encode():
 	plainMsg = sys.argv[1] # Read plain message
 	key = decimal.Decimal(int(sys.argv[2])) # Read key
-	key = str(key.sqrt()).split('.')[1] #
-	encriptedMsg = ' '
+	key = str(key.sqrt()).split('.')[1] # Get 1000 digits of the square root
 	
-	for c in plainMsg:
-		if(c == ' '):
-			encriptedMsg += ' '
-		else:
+	file = open("input.txt","w")
 
+	for c, i in zip(plainMsg, key):
+		file.write(chr( (ord(c) + int(i))%256 ))
+
+	file.close()
 
 encode()
--- project1/pseudoRandomGenerator.py
+++ project1/pseudoRandomGenerator.py
@@ -4,7 +4,7 @@ import sys
 def generate():
 	file = open("input.txt","w") 
 	n = int(sys.argv[1])
-
+	
 	for i in range(n):
 		x = random.randint(0,9)
 		file.write(str(x))
