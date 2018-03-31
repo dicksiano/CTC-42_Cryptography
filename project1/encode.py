3da45c1699b5db7511ab76c920e66a5d8327a752
Dicksiano <dicksianomelo@gmail.com>
2018-03-30 22:20:54 -0300

    working on encode

--- project1/encode.py
+++ project1/encode.py
@@ -1,83 +1,36 @@
 import sys
 import math
 import decimal
-
+import random
 
 context = decimal.Context(prec=1001001)
 decimal.setcontext(context)
 
-fibonacciNumbers = [0,1,2,3,5,8,13,21,34,55]
-primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
-73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,
-179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,
-283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,
-419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541
-547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,
-661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,
-811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,
-947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,
-1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,1153,1163,1171,1181,1187,1193,1201,1213,1217,1223,
-1229,1231,1237,1249,1259,1277,1279,1283,1289,1291,1297,1301,1303,1307,1319,1321,1327,1361,1367,1373,
-1381,1399,1409,1423,1427,1429,1433,1439,1447,1451,1453,1459,1471,1481,1483,1487,1489,1493,1499,1511,
-1523,1531,1543,1549,1553,1559,1567,1571,1579,1583,1597,1601,1607,1609,1613,1619,1621,1627,1637,1657,
-1663,1667,1669,1693,1697,1699,1709,1721,1723,1733,1741,1747,1753,1759,1777,1783,1787,1789,1801,1811,
-1823,1831,1847,1861,1867,1871,1873,1877,1879,1889,1901,1907,1913,1931,1933,1949,1951,1973,1979,1987,
-1993,1997,1999,2003,2011,2017,2027,2029,2039,2053,2063,2069,2081,2083,2087,2089,2099,2111,2113,2129,
-2131,2137,2141,2143,2153,2161,2179,2203,2207,2213,2221,2237,2239,2243,2251,2267,2269,2273,2281,2287,
-2293,2297,2309,2311,2333,2339,2341,2347,2351,2357,2371,2377,2381,2383,2389,2393,2399,2411,2417,2423,
-2437,2441,2447,2459,2467,2473,2477,2503,2521,2531,2539,2543,2549,2551,2557,2579,2591,2593,2609,2617,
-2621,2633,2647,2657,2659,2663,2671,2677,2683,2687,2689,2693,2699,2707,2711,2713,2719,2729,2731,2741,
-2749,2753,2767,2777,2789,2791,2797,2801,2803,2819,2833,2837,2843,2851,2857,2861,2879,2887,2897,2903,
-2909]
+def generateFirstTrash(initialPos):
+	trash = ''
 
-def getBit(n, x):
-	if (n & (1 << x)):
-		return 1
-	return 0
+	for x in range(initialPos - 1):
+		trash += chr(random.randint(0,255))
 
-assert getBit(1, 0) == 1
-assert getBit(2, 0) == 0
-assert getBit(2, 1) == 1
-assert getBit(4, 2) == 1
-assert getBit(8, 3) == 1
-assert getBit(1, 1) == 0
-assert getBit(2, 2) == 0
-assert getBit(4, 3) == 0
+	return trash
 
-def getSecretNum(msg, n, x):
-	answer = 0
-	for i in range(n):
-		num = ord(msg[fibonacciNumbers[i]])
-		answer |= getBit(num, x) << i
-		num = ord(msg[ len(msg) - 1 - fibonacciNumbers[i] ])
-		answer |= getBit(num, x) << (n + i)
-	
-	return answer
-		
-assert getSecretNum("aaox@r@@w@@@@w@@r@xoaa",6,0) == 2535
+assert len(generateFirstTrash(5)) == 4
 
-def getInitialPos(msg)
-	return getSecretNum(msg, 7, 0)
+def generateSecondTrash(initialPos, msgSize):
+	trash = ''
 
-def getMsgSize(msg):
-	return getSecretNum(msg, 5, 1)
+	for x in range(initialPos + msgSize, 10000):
+		trash += chr(random.randint(0,255))
 
-def getKey(msg):
-	return primeNumbers[getSecretNum(msg, 5, 2)]
+	return trash
 
-def getInitilKey(msg):
-	return getSecretNum(msg, 10, 3)
+assert len(generateSecondTrash(8990, 1000)) == 10
 
-def encode():
-	plainMsg = sys.argv[1] # Read plain message
-	key = decimal.Decimal(int(sys.argv[2])) # Read key
-	key = str(key.sqrt()).split('.')[1] # Get 1000 digits of the square root
-	
-	file = open("input.txt","w")
+def encode(plainText, key):
+	encriptedMsg = ''
+	for c, i in zip(plainText, key):
+		encriptedMsg += chr( (ord(c) + int(i))%256 )
 
-	for c, i in zip(plainMsg, key):
-		file.write(chr( (ord(c) + int(i))%256 ))
+	return encriptedMsg
 
-	file.close()
-
-encode()
+assert encode("abc", "205") == "cbh"
