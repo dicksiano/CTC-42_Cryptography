0984f86176e6947185966c7dca2a242d6a59b98a
Dicksiano <dicksianomelo@gmail.com>
2018-03-30 21:30:19 -0300

    decode working

--- project1/decode.py
+++ project1/decode.py
@@ -1,20 +1,68 @@
 import sys
 import math
 import decimal
+from utils import fibonacciNumbers, getBit, primeNumbers
 
 context = decimal.Context(prec=1001)
 decimal.setcontext(context)
 
-def encode():
-	plainMsg = sys.argv[1] # Read plain message
-	key = decimal.Decimal(int(sys.argv[2])) # Read key
-	key = str(key.sqrt()).split('.')[1] # Get 1000 digits of the square root
+def getSecretNum(msg, n, x):
+	answer = 0
+	for i in range(n):
+		num = ord(msg[fibonacciNumbers[i]])
+		answer |= getBit(num, x) << i
+		num = ord(msg[ len(msg) - 1 - fibonacciNumbers[i] ])
+		answer |= getBit(num, x) << (n + i)
 	
-	file = open("input.txt","w")
+	return answer
+		
+assert getSecretNum("aaox@r@@w@@@@w@@r@xoaa",6,0) == 2535
 
-	for c, i in zip(plainMsg, key):
-		file.write(chr( (ord(c) - int(i))%256 ))
+def getInitialPos(msg):
+	return getSecretNum(msg, 7, 0)
 
-	file.close()
+def getMsgSize(msg):
+	return getSecretNum(msg, 5, 1)
 
-encode()
+def getKey(msg):
+	return primeNumbers[getSecretNum(msg, 5, 2)]
+
+def getInitilPosKey(msg):
+	return getSecretNum(msg, 10, 3)
+
+def getParams(msg): 
+	return [getInitialPos(msg), getMsgSize(msg), getKey(msg), getInitilPosKey(msg)]
+
+def generateFinalKey(key, initialPosKey, keySize):
+	key = decimal.Decimal(primeNumbers[key]) # Read key
+	key = str(key.sqrt()).split('.')[1]
+	return key[initialPosKey:(initialPosKey+keySize)]
+	
+assert generateFinalKey(1,2,2) == "42"
+assert generateFinalKey(0,0,5) == "77245"
+
+def getEncriptedMsg(msg, initialPos, msgSize):
+	return msg[initialPos:(initialPos+msgSize)]
+
+assert getEncriptedMsg("q393asasdcnjanaaussafasufdicksianoasasnfjanjas", 25, 9) == "dicksiano"
+
+def decode(msg):
+	[initialPos, msgSize, key, initilPosKey] = getParams(msg)
+	key = generateFinalKey(key, initilPosKey,msgSize)
+	encriptedMsg = getEncriptedMsg(msg, initialPos, msgSize)
+
+	return decode(encriptedMsg, key)
+
+def decode(encriptedMsg, key):
+	decriptedMsg = ''
+	for c, i in zip(encriptedMsg, key):
+		decriptedMsg += chr( (ord(c) - int(i))%256 )
+	
+	return decriptedMsg
+
+assert decode("cbh", "205") == "abc"
+
+	
+
+
+
--- project1/utils.py
+++ project1/utils.py
@@ -1,17 +1,11 @@
-import sys
 import math
-import decimal
-
-
-context = decimal.Context(prec=1001001)
-decimal.setcontext(context)
 
 fibonacciNumbers = [0,1,2,3,5,8,13,21,34,55]
-primeNumbers = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
+primeNumbers = [math.pi,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
 73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,
 179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,
 283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,
-419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541
+419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,
 547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,
 661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,
 811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,
@@ -44,40 +38,3 @@ assert getBit(1, 1) == 0
 assert getBit(2, 2) == 0
 assert getBit(4, 3) == 0
 
-def getSecretNum(msg, n, x):
-	answer = 0
-	for i in range(n):
-		num = ord(msg[fibonacciNumbers[i]])
-		answer |= getBit(num, x) << i
-		num = ord(msg[ len(msg) - 1 - fibonacciNumbers[i] ])
-		answer |= getBit(num, x) << (n + i)
-	
-	return answer
-		
-assert getSecretNum("aaox@r@@w@@@@w@@r@xoaa",6,0) == 2535
-
-def getInitialPos(msg)
-	return getSecretNum(msg, 7, 0)
-
-def getMsgSize(msg):
-	return getSecretNum(msg, 5, 1)
-
-def getKey(msg):
-	return primeNumbers[getSecretNum(msg, 5, 2)]
-
-def getInitilKey(msg):
-	return getSecretNum(msg, 10, 3)
-
-def encode():
-	plainMsg = sys.argv[1] # Read plain message
-	key = decimal.Decimal(int(sys.argv[2])) # Read key
-	key = str(key.sqrt()).split('.')[1] # Get 1000 digits of the square root
-	
-	file = open("input.txt","w")
-
-	for c, i in zip(plainMsg, key):
-		file.write(chr( (ord(c) + int(i))%256 ))
-
-	file.close()
-
-encode()
